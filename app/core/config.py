d4ba2ae02ea16930055c375f211c09ba189b4b14
Srivaths <srivaths.iyer@kreedalabs.com>
2025-12-11 23:40:06 +0530

    Readme Updates

--- app/core/config.py
+++ app/core/config.py
@@ -1,21 +1,16 @@
-# app/core/config.py
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    # App Config
     PROJECT_NAME: str = "Organization Management Service"
     API_V1_STR: str = "/api/v1"
     
-    # Security Config (In production, load these from .env)
-    SECRET_KEY: str = "super_secret_key_for_assignment_only"
+    SECRET_KEY: str 
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
-    # Database Config
-    MONGO_URL: str = "mongodb+srv://sriKreeda:backendPass@cluster0.cbd9tlw.mongodb.net/?appName=Cluster0"
+    MONGO_URL: str
     MASTER_DB_NAME: str = "master_metadata"
 
-    class Config:
-        case_sensitive = True
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
